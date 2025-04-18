b8848bafc545bc4f09cae84c0779e66604400d9a
unknown <ghycinth9@gmail.com>
2025-04-18 01:38:37 +0100

    first commit

--- src/components/socket.js
+++ src/components/socket.js
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 
 // const socket = io("https://videoconferencingbackend-xkva.onrender.com"); // use Render URL here
 
-const socket = io("ttps://videoconferencingbackend-xkva.onrender.com", {
+const socket = io("https://videoconferencingbackend-xkva.onrender.com", {
     transports: ["websocket"],
     reconnection: true,
   });
